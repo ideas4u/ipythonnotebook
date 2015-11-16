1ae422af1a12bd1d0bafefe03cc15b8bb1282728
ideas4u <xiewygz@m.scnu.edu.cn>
2015-11-16 16:38:32 +0800

    add the modify of test.py file to repo at testbranch

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-print("This is a test file.")
 print("I love coding, it can use for my job.")
 print("I can write script to help with my job done.")
+
+print("just now i have delected the first line.and add this line.")
+print("Modify this time is in the testbranch")
 
 
